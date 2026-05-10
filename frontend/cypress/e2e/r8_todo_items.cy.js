14ffd80653c6731452eb794772f6608195efadd0
omposs <omposs@proton.me>
2026-05-10 17:45:14 +0200

    Added Cypress tests for R8 todo items

---
+++ frontend/cypress/e2e/r8_todo_items.cy.js
@@ -0,0 +1,101 @@
+describe('R8 - Todo items', () => {
+  // define variables that we need on multiple occasions
+  let uid // user id
+  let name // name of the user (firstName + ' ' + lastName)
+  let email // email of the user
+
+  before(function () {
+    // create a fabricated user from a fixture
+    cy.fixture('user.json')
+      .then((user) => {
+        cy.request({
+          method: 'POST',
+          url: 'http://localhost:5000/users/create',
+          form: true,
+          body: user
+        }).then((response) => {
+          uid = response.body._id.$oid
+          name = user.firstName + ' ' + user.lastName
+          email = user.email
+        })
+      })
+  })
+
+  // open the website and log in to the dashboard
+  function loginToSystem(email, name) {
+    cy.visit('http://localhost:3000')
+
+    cy.contains('div', 'Email Address')
+      .find('input[type=text]')
+      .type(email)
+
+    cy.get('form')
+      .submit()
+
+    cy.get('h1')
+      .should('contain.text', 'Your tasks, ' + name)
+  }
+
+  // create a task
+  function createTask() {
+    cy.get('input[placeholder="Title of your Task"]')
+      .type('Test Task')
+
+    cy.get('input[placeholder*="Viewkey"]')
+      .type('dQw4w9WgXcQ')
+
+    cy.get('input[type="submit"][value="Create new Task"]')
+      .click()
+
+    cy.get('img')
+      .first()
+      .click()
+
+    cy.get('input[placeholder="Add a new todo item"]')
+      .should('be.visible')
+  }
+
+  // automate tests by using cypress
+  it('R8 tests: creates, toggles, and deletes a todo item', () => {
+    loginToSystem(email, name)
+    createTask()
+
+    // R8UC1: create a todo item
+    cy.get('input[placeholder="Add a new todo item"]')
+      .type('Rickroll')
+
+    cy.get('input[type="submit"][value="Add"]')
+      .click()
+
+    cy.contains('li.todo-item', 'Rickroll')
+      .should('be.visible')
+
+    // R8UC2: toggle the todo item
+    cy.contains('li.todo-item', 'Rickroll')
+      .find('span.checker.unchecked')
+      .click()
+
+    cy.contains('li.todo-item', 'Rickroll')
+      .find('span.checker.checked')
+      .should('be.visible')
+
+    // R8UC3: delete the todo item
+    cy.contains('li.todo-item', 'Rickroll')
+      .find('span.remover')
+      .click()
+
+    cy.contains('li.todo-item', 'Rickroll')
+      .should('not.exist')
+  })
+
+  after(function () {
+    // clean up by deleting the user from the database
+    cy.request({
+      method: 'DELETE',
+      url: `http://localhost:5000/users/${uid}`,
+      failOnStatusCode: false
+    }).then((response) => {
+      cy.log(response.body)
+    })
+  })
+})
